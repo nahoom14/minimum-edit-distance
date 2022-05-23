a4910bc20839230c11d4099a45a378555a984e0c
Nahoom Belete <nahoombelete22@gmail.com>
2022-05-23 19:58:02 +0200

    Add comments

--- med.py
+++ med.py
@@ -1,14 +1,17 @@
-def min_edit_distance(target, source):
-    n = len(target)
-    m = len(source)
+#returns minimum edit distance of two words
+def min_edit_distance(source, target):
+    n = len(source)
+    m = len(target)
 
+    #create 2d matrix adding extra cell for empty string
     distance = [[0 for i in range(n+1)] for j in range(m+1)]
 
-    
+    #assign values to first row and column
     for k in range(n + 1):
         distance[k][0] = k
         distance[0][k] = k
     
+    #print matrix
     for row in distance:
         print(row)
 
@@ -19,6 +22,7 @@ def min_edit_distance(target, source):
 # print("Enter target word:")
 # s = input()
 
+#client code
 t, s = "intention", "detention"
 
 min_edit_distance(t, s)
